b541f24230123c4ccd5a590291c5f830c48986b7
Michel Megens <dev@bietje.net>
2017-12-04 16:51:23 +0100

    estack: add error definitions
    Add a list of error definitions in the `<estack/error.h>' header.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ include/estack/error.h
@@ -0,0 +1,18 @@
+/*
+ * E/STACK error codes
+ *
+ * Author: Michel Megens
+ * Date: 04/12/2017
+ * Email: dev@bietje.net
+ */
+
+#ifndef __ESTACK_ERROR_H__
+#define __ESTACK_ERROR_H__
+
+typedef enum error_code {
+	EOK = 0,
+	EDRPPPED,
+	EARRIVED
+} error_t;
+
+#endif // !__ESTACK_ERROR_H__
