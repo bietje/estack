7c35cae00e64c2f6a3724e249d09563c5dda26bb
Michel Megens <dev@bietje.net>
2017-12-05 14:12:52 +0100

    tools: add a tools subsystem with byteorder tool
    Add a tools subystem with a tool to to convert numerical values from
    network to host endianess and visa versa.
    
    The tools subsystem contains an implementation of getopt for windows systems.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ tools/getopt.h
@@ -0,0 +1,15 @@
+/*
+ * E/STACK byte order tool
+ *
+ * Author: Michel Megens
+ * Date: 28/11/2017
+ * Email: dev@bietje.net
+ */
+
+#ifndef __GETOPT_H__
+#define __GETOPT_H__
+
+extern char *optarg; 
+extern int optind, opterr, optopt;
+
+#endif // !__GETOPT_H__
