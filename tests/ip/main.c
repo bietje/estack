a6c9b105479816cd647501a8ddf48e8964a56fd9
Michel Megens <dev@bietje.net>
2017-12-12 14:19:06 +0100

    test: ip: add an IP unit test
    Add a unit test for the IP and ICMP protocol.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ tests/ip/main.c
@@ -0,0 +1,80 @@
+/*
+ * ARP unit test
+ *
+ * Author: Michel Megens
+ * Date:   07/12/2017
+ * Email:  dev@bietje.net
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <pcap.h>
+#include <estack.h>
+#include <string.h>
+
+#include <estack/netbuf.h>
+#include <estack/ethernet.h>
+#include <estack/netdev.h>
+#include <estack/pcapdev.h>
+#include <estack/error.h>
+#include <estack/list.h>
+#include <estack/arp.h>
+
+#ifdef WIN32
+#include <Windows.h>
+#endif
+
+#ifndef WIN32
+typedef unsigned char u_char;
+#endif
+
+static int err_exit(int code, const char *fmt, ...)
+{
+	va_list va;
+
+	va_start(va, fmt);
+	vfprintf(stderr, fmt, va);
+	va_end(va);
+
+	getchar();
+	exit(code);
+}
+
+static void print_dst_cache(struct netdev *dev)
+{
+}
+
+#define HW_ADDR1 {0xf0, 0xf7, 0x55, 0xbd, 0xbe, 0x40}
+static const uint8_t hw1[] = HW_ADDR1;
+
+#define HW_ADDR {0x48, 0x5D, 0x60, 0xBF, 0x51, 0xA9}
+
+int main(int argc, char **argv)
+{
+	char *input;
+	struct netdev *dev;
+	const uint8_t hwaddr[] = HW_ADDR;
+	uint32_t addr;
+
+	if (argc < 2) {
+		err_exit(-EXIT_FAILURE, "Usage: %s <input-file>\n", argv[0]);
+	} else {
+		input = argv[1];
+	}
+
+	estack_init(NULL);
+
+	dev = pcapdev_create(input, "ip-output.pcap", hwaddr, 1500);
+	netdev_config_params(dev, 30, 15000);
+	pcapdev_create_link_ip4(dev, 0x9131060C, 0, 0xFFFFC000);
+
+	addr = ipv4_atoi("145.49.63.254");
+	netdev_add_destination(dev, hw1, ETHERNET_MAC_LENGTH, (void*)&addr, 4);
+
+	netdev_poll(dev);
+
+	getchar();
+	return 0;
+}
+
