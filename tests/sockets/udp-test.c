f5106a45866c526927fe6210d83f4e6fca124710
Michel Megens <dev@bietje.net>
2018-01-08 19:29:10 +0100

    ci: add the UDP socket test to the CI config
    Add the UDP socket test and the events unit test to the CI configuration
    file.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- tests/sockets/udp-test.c
+++ tests/sockets/udp-test.c
@@ -8,14 +8,33 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <estack.h>
 
+#include <estack/test.h>
 #include <estack/socket.h>
+
+static int err_exit(int code, const char *fmt, ...)
+{
+	va_list va;
+
+	va_start(va, fmt);
+	vfprintf(stderr, fmt, va);
+	va_end(va);
+
+	wait_close();
+	exit(code);
+}
 
 int main(int argc, char **argv)
 {
+	if (argc < 2) {
+		err_exit(-EXIT_FAILURE, "Usage: %s <input-file>\n", argv[0]);
+	}
+
 	estack_init(stdout);
 	estack_destroy();
 
+	wait_close();
 	return -EXIT_SUCCESS;
 }
