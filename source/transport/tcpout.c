c4d62edf5d193edcfc015ebbd45352022e9d2ec9
Michel Megens <dev@bietje.net>
2018-01-18 12:04:22 +0100

    net: tcp: implement TCP data structures
    Implement the TCP header and TCP protocol control block. The TCP PCB is
    responsible for keeping track of the TCP connection state and takes care
    of all background tasks (such as keep alive and retransmissions) using
    timers.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ source/transport/tcpout.c
@@ -0,0 +1,14 @@
+/*
+ * E/STACK - TCP output
+ *
+ * Author: Michel Megens
+ * Date: 18/01/2017
+ * Email: dev@bietje.net
+ */
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <estack.h>
+
+#include <estack/error.h>
+#include <estack/tcp.h>
