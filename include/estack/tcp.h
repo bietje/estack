77ec036dcb9b4c9d52844fb1e8418e7283962ed5
Michel Megens <dev@bietje.net>
2018-01-19 11:58:00 +0100

    net: tcp: add the tcp pcb definition to tcp.h
    Implement the TCP protocol control block. The protocol control block a a
    datastructure that functions as the backend for a TCP stream. It keeps
    track of data, acknowledgements and retransmissions.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ include/estack/tcp.h
@@ -0,0 +1,75 @@
+/*
+ * E/STACK - TCP definitions
+ * 
+ * Author: Michel Megens
+ * Email:  dev@bietje.net
+ * Date:   19/01/2018
+ */
+
+#ifndef __TCP_H__
+#define __TCP_H__
+
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <estack/estack.h>
+#include <estack/socket.h>
+#include <estack/list.h>
+
+#pragma pack(push, 1)
+struct tcp_hdr {
+	uint16_t sport;
+	uint16_t dport;
+	uint32_t seq_no;
+	uint32_t ack_no;
+	uint16_t data_ofs;
+	uint16_t window;
+	uint16_t checksum;
+	uint16_t urg_ptr;
+};
+#pragma pack(pop)
+
+typedef enum {
+	TCP_CLOSED,
+	TCP_LISTEN,
+	TCP_SYN_SENT,
+	TCP_SYN_RECEIVED,
+	TCP_ESTABLISHED,
+	TCP_FIN_WAIT_1,
+	TCP_FIN_WAIT_2,
+	TCP_CLOSE_WAIT,
+	TCP_CLOSING,
+	TCP_LAST_ACK,
+	TCP_TIME_WAIT,
+} tcp_state_t;
+
+struct tcp_pcb {
+	struct socket sock;
+	tcp_state_t state;
+	struct netdev *dev;
+
+	estack_timer_t *keepalive;
+	estack_timer_t *rtx;
+
+	uint32_t iss;
+
+	uint32_t rcv_next;
+	uint32_t rcv_window;
+	uint32_t rcv_window_announce;
+
+	uint32_t snd_unack; /* Oldest unacknowledged sequence number */
+	uint32_t snd_next;
+	uint32_t snd_window;
+	uint32_t snd_wu_seq; //!< Sequence number of last window update
+	uint32_t snd_wu_ack; //!< Ack number of last window update
+
+	int16_t rto; //!< RTT timeout.
+	int32_t rttvar; //!< RTT variance.
+	int32_t srtt; //!< Smoothed round trip time.
+
+	struct list_head snd_q;
+	struct list_head unack_q;
+	struct list_head oos_q;
+};
+
+#endif
---
+++ source/transport/tcp-out.c
@@ -0,0 +1,12 @@
+/*
+ * E/STACK - TCP
+ * 
+ * Author: Michel Megens
+ * Date:   19/01/2018
+ * Email:  dev@bietje.net
+ */
+
+#include <estack/estack.h>
+#include <estack/list.h>
+#include <estack/socket.h>
+#include <estack/tcp.h>
